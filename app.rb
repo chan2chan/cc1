021acf1b5f4228cd9d44bbdda750ae3eb3c2fa2c
Chan Chan <chan2chan.2009@gmail.com>
2017-07-14 03:49:46 +0000

    Update app.rb

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ get '/test/push' do
     userId = ENV["LINE_TEST_USER_ID"]
     message = {
         type: 'text',
-        text: 'အွန်လိုင်းအမှာစာတစ်ဆောင် လက္ခံရရှိပါတယ် ခင်ညd'
+        text: 'အွန်လိုင်းအမှာစာတစ်ဆောင် လက္ခံရရှိပါတယ် ခင်ည'
     }
     response = client.push_message(userId, message)
     p "#{response.code} #{response.body}"
